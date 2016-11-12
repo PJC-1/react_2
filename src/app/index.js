8bf2261cb6bf70e8179d949cb0d5a9bb68af60d1
PJC-1 <jumpzine@gmail.com>
2016-11-11 22:09:16 -0800

    added a simple component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+// Create component
+var TodoComponent = React.createClass({
+    render: function(){
+      return(
+        <h1>Helllloooo</h1>
+      );
+    }
+});
+
+// put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
