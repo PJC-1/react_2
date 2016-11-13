d748cd3da0b7f5bdeba9bff8a0acf24e2a821534
PJC-1 <jumpzine@gmail.com>
2016-11-13 14:03:33 -0800

    added another nested component for practice

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,8 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
     getInitialState: function(){
         return {
-            todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers']
+            todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers'],
+            pokemon: ['palossand', 'alakazam', 'decidueye', 'vikavolt', 'alola nine-tails', 'mimikyu']
         }
     },
     render: function(){
@@ -13,18 +14,52 @@ var TodoComponent = React.createClass({
       var todos = this.state.todos;
       todos = todos.map(function(item,index){
         return(
-            <li>{item}</li>
+          <TodoItem item={item} key={index}/>
+        );
+      })
+      var pokemon = this.state.pokemon;
+      pokemon = pokemon.map(function(item,index){
+        return(
+          <PokemonItem item={item} key={index}/>
         );
       })
       return(
           <div id="todo-list">
               <p>The busiest people have the most leisure...</p>
               <ul>{todos}</ul>
+              <br></br>
+              <p>Pokemon I want to use in sun and moon...</p>
+              <ul>{pokemon}</ul>
           </div>
       );
     }// render
 });
 
+// create TodoItem component
+var TodoItem = React.createClass({
+  render: function(){
+    return(
+      <li>
+          <div className="todo-item">
+              <span className="item-name">{this.props.item}</span>
+          </div>
+      </li>
+    );
+  }// render
+});
+
+// create PokemonItem component
+var PokemonItem = React.createClass({
+  render: function(){
+    return(
+      <li>
+          <div className="pokemon-item">
+              <span className="item-name">{this.props.item}</span>
+          </div>
+      </li>
+    )
+  }// render
+});
 
 // we add props into a component here in the 'component tag' and then we can
 // reference them from within the component above.
