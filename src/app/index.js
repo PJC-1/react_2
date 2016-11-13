65e1da9b09eab86df78e22aad789f23c1de4c9d1
PJC-1 <jumpzine@gmail.com>
2016-11-12 16:33:43 -0800

    added simple example of state

--- src/app/index.js
+++ src/app/index.js
@@ -3,10 +3,20 @@ var ReactDOM = require('react-dom');
 
 
 var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['wash up', 'eat some cheese', 'take a nap']
+        }
+    },
     render: function(){
       return(
-          <div>
-          
+          <div id="todo-list">
+              <p>The busiest people have the most leisure...</p>
+              <ul>
+                  <li>{this.state.todos[0]}</li>
+                  <li>{this.state.todos[1]}</li>
+                  <li>{this.state.todos[2]}</li>
+              </ul>
           </div>
       );
     }// render
